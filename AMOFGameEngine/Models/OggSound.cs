06a1dabb4bc0d6b540fd320bdf55b09151c0bb23
cook <dell20091234@gmail.com>
2016-11-20 13:09:35 +0800

    Update OggSound Wrapper

--- AMOFGameEngine/Models/OggSound.cs
+++ AMOFGameEngine/Models/OggSound.cs
@@ -7,39 +7,58 @@ using NVorbis;
 
 namespace AMOFGameEngine
 {
+    enum OGGSTATUS
+    {
+        OGGS_STOP,
+        OGGS_PLAYING,
+        OGGS_PAUSE
+    }
+
     class OggSound
     {
         private NAudio.Vorbis.VorbisWaveReader _vorbis;
         private NAudio.Wave.WaveOut _waveout;
-        public string OggFileName;
+        private string _oggfilename;
+        public string OggFileName
+        {
+            get 
+            {
+                return _oggfilename;
+            }
+            set 
+            {
+                _oggfilename = value;
+            }
+        }
+        private int _oggstatus;
+        public int OggStatus
+        {
+            get
+            {
+                return _oggstatus;
+            }
+        }
         public OggSound()
         {
             _waveout = null;
             _vorbis = null;
+            _oggstatus = (int)OGGSTATUS.OGGS_STOP;
         }
-        /*public OggSound Singleton
-        {
-            get
-            {
-                if (ogginstance == null)
-                {
-                    ogginstance = new OggSound();
-                }
-                return ogginstance;
-            }
-        }*/
         public void PlayOgg()
-        {;
-            _vorbis = new NAudio.Vorbis.VorbisWaveReader(OggFileName);
+        {
+            _vorbis = new NAudio.Vorbis.VorbisWaveReader(_oggfilename);
             _waveout = new NAudio.Wave.WaveOut();
             _waveout.Init(_vorbis);
             _waveout.Play();
+            _oggstatus = (int)OGGSTATUS.OGGS_PLAYING;
+
         }
         public void PauseOgg()
         {
             if (_waveout != null && _vorbis != null)
             {
                 _waveout.Pause();
+                _oggstatus = (int)OGGSTATUS.OGGS_PAUSE;
             }
         }
         public void StopOgg()
@@ -47,6 +66,7 @@ namespace AMOFGameEngine
             if(_waveout!=null && _vorbis!=null)
             {
                 _waveout.Stop();
+                _oggstatus = (int)OGGSTATUS.OGGS_STOP;
             }
         }
     }
