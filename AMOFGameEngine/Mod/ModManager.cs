9da8b3fb8a2d6cb4994bca64d55c82ec1a95645c
cook <dell20091234@gmail.com>
2017-03-11 11:58:58 +0800

    First Define Mod Format

---
+++ AMOFGameEngine/Mod/ModManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace AMOFGameEngine.Mod
+{
+    class ModManager
+    {
+        const string modPath = Directory.Exists("./mods")?"./mods":
+            "C:\\Users\\Administrator\\Documents\\AMOFGameEngine\\mods";
+        const string modFileName = "mod.xml";
+
+        List<string> avaliableModNames;
+        public List<string> AvaliableModNames
+        {
+            get { return avaliableModNames; }
+            set { avaliableModNames = value; }
+        }
+
+        ModManager()
+        {
+            avaliableModNames = new List<string>();
+        }
+
+        public void InitMod()
+        {
+            string modXMLLocation = modPath + "/mod.xml";
+            string[] modDirs = Directory.GetDirectories(modPath);
+            foreach (string modDir in modDirs)
+            {
+                if (File.Exists(string.Format("{0}/{1}/{2}", modPath, modDir, modFileName)))
+                {
+                    avaliableModNames.Add(modDir);
+                }
+            }
+        }
+
+        void ProcessModFiles()
+        {
+
+        }
+    }
+}
---
+++ AMOFGameEngine/States/ModChooser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AMOFGameEngine.States
+{
+    public class ModChooser : AppState
+    {
+        public ModChooser()
+        {
+
+        }
+
+        public override void enter()
+        {
+            base.enter();
+        }
+
+        public override bool pause()
+        {
+            return base.pause();
+        }
+
+        public override void resume()
+        {
+            base.resume();
+        }
+
+        public override void exit()
+        {
+            base.exit();
+        }
+
+        public override void update(double timeSinceLastFrame)
+        {
+            base.update(timeSinceLastFrame);
+        }
+    }
+}
