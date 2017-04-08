d1f5b7546aad689bcc0ae5b3ab0c85292aba14a0
cook <dell20091234@gmail.com>
2017-04-08 20:02:37 +0800

    Update the Project

---
+++ AMOFGameEngine.Mods.Sample/ModSample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AMOFGameEngine.Mods.Common;
+
+namespace AMOFGameEngine.Mods.Sample
+{
+    class ModSample : Mod
+    {
+    }
+}
