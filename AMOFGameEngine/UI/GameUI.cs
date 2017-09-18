d655cc77b705b2d80eb30ebc2c22b2da05683b16
cook <dell20091234@gmail.com>
2017-09-18 23:09:59 +0800

    GameUI

---
+++ AMOFGameEngine/UI/GameUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mogre;
+using Mogre_Procedural.MogreBites;
+
+namespace AMOFGameEngine.UI
+{
+    public class GameUI : SdkTrayListener
+    {
+        public GameUI()
+        {
+        }
+
+        public virtual void Show() { }
+
+        public virtual void Close() { }
+    }
+}
---
+++ AMOFGameEngine/UI/GameUIManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mogre;
+using AMOFGameEngine.Widgets;
+
+namespace AMOFGameEngine.UI
+{
+    public class GameUIManager
+    {
+        private List<KeyValuePair<string,GameUI>> registeredUI;
+        private Stack<KeyValuePair<string,GameUI>> runningUI;
+
+        public GameUIManager()
+        {
+            registeredUI = new List<KeyValuePair<string, GameUI>>();
+            runningUI = new Stack<KeyValuePair<string, GameUI>>();
+        }
+
+        private static GameUIManager instance;
+        public static GameUIManager Singleton
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new GameUIManager();
+                }
+                return instance;
+            }
+        }
+
+        public void RegisterUI(string name,GameUI ui)
+        {
+            KeyValuePair<string, GameUI> info = new KeyValuePair<string, GameUI>
+            (name, ui);
+            registeredUI.Add(info);
+        }
+
+        public void CreateUI(string name)
+        {
+            GameUI ui= registeredUI.Where(o => o.Key == name).First().Value;
+            KeyValuePair<string, GameUI> info = new KeyValuePair<string, GameUI>
+            (name, ui);
+            runningUI.Push(info);
+            runningUI.Peek().Value.Show();
+        }
+
+        public void ChangeUI(string name)
+        {
+            GameUI ui = registeredUI.Where(o => o.Key == name).First().Value;
+            KeyValuePair<string, GameUI> info = new KeyValuePair<string, GameUI>
+            (name, ui);
+            runningUI.Push(info);
+            runningUI.Peek().Value.Show();
+        }
+
+        public void CloseUI(string name)
+        {
+            GameUI ui = registeredUI.Where(o => o.Key == name).First().Value;
+            KeyValuePair<string, GameUI> info = new KeyValuePair<string, GameUI>
+            (name, ui);
+            runningUI.Push(info);
+            runningUI.Pop().Value.Close();
+        }
+    }
+}
