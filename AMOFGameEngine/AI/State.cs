cf41875fce4a86321944fc09f896797ecea79d9c
cook <dell20091234@gmail.com>
2017-11-04 11:25:53 +0800

    State Machine

---
+++ AMOFGameEngine/AI/State.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AMOFGameEngine.RPG;
+
+namespace AMOFGameEngine.AI
+{
+    public class State<T> where T : RPGObject
+    {
+        private static State<T> instance;
+        public static State<T> Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new State<T>();
+                }
+                return instance;
+            }
+        }
+        public virtual void Enter(T owner) { }
+        public virtual void Execute(T owner) { }
+        public virtual void Exit(T owner) { }
+    }
+}
---
+++ AMOFGameEngine/AI/StateMachine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AMOFGameEngine.RPG;
+
+namespace AMOFGameEngine.AI
+{
+    public class StateMachine<T> where T : RPGObject
+    {
+        private T owner;
+        public T Owner { get { return owner; } }
+        public State<T> CurrentState { get; set; }
+        public State<T> PreviousState { get; set; }
+        public State<T> GlobalState { get; set; }
+
+        public StateMachine(T owner)
+        {
+            this.owner = owner;
+            CurrentState = null;
+            PreviousState = null;
+            GlobalState = null;
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
