866c1a79d34f0fe1e6fc43727b89dd8656e3f708
cook <dell20091234@gmail.com>
2017-12-15 22:32:12 +0800

    New Addon System

---
+++ AMOFGameEngine/Addon/Addon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AMOFGameEngine.Addon
+{
+    public interface Addon
+    {
+        void StartAddon();
+
+        void StopAddon();
+    }
+}
