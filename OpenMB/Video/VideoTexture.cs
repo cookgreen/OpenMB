d92cdfd7dfce3c75c699294e3637822b14458643
Cook Green <dell20091234@gmail.com>
2019-06-23 09:56:47 +0800

    Update Video Texture

--- OpenMB/Video/VideoTexture.cs
+++ OpenMB/Video/VideoTexture.cs
@@ -13,22 +13,24 @@ namespace OpenMB.Video
         public VideoStream Stream;
         public int FrameNum;
         private ManualObject screen;
+        private TexturePtr videoTex;
+        private MaterialPtr videoMat;
 
         public VideoTexture(SceneManager scm, float width,float height, string aviFileName)
         {
             AviManager aviMgr = new AviManager(aviFileName, true);
             Stream = aviMgr.GetVideoStream();
 
-            TexturePtr VideoTexture = TextureManager.Singleton.CreateManual(
+            videoTex = TextureManager.Singleton.CreateManual(
                 "Video",
                 ResourceGroupManager.DEFAULT_RESOURCE_GROUP_NAME,
                 TextureType.TEX_TYPE_2D, Convert.ToUInt32(Stream.Width), Convert.ToUInt32(Stream.Height), 0, PixelFormat.PF_R8G8B8A8, (int)TextureUsage.TU_DYNAMIC_WRITE_ONLY_DISCARDABLE);
-            MaterialPtr VideoMat = MaterialManager.Singleton.Create(
+            videoMat = MaterialManager.Singleton.Create(
                 "VideoMat", ResourceGroupManager.DEFAULT_RESOURCE_GROUP_NAME);
-            VideoMat.GetTechnique(0).GetPass(0).LightingEnabled = false ;
-            VideoMat.GetTechnique(0).GetPass(0).CreateTextureUnitState("Video");
+            videoMat.GetTechnique(0).GetPass(0).LightingEnabled = false ;
+            videoMat.GetTechnique(0).GetPass(0).CreateTextureUnitState("Video");
 
-            PixelBuffer = VideoTexture.GetBuffer();
+            PixelBuffer = videoTex.GetBuffer();
 
             screen = scm.CreateManualObject("Screen");
             screen.Dynamic = true;
@@ -70,6 +72,14 @@ namespace OpenMB.Video
         {
             Stream.GetFrameClose();
             PixelBuffer.Dispose();
+            screen.Dispose();
+            videoMat.Dispose();
+            videoTex.Dispose();
+
+            videoTex = null;
+            videoMat = null;
+            screen = null;
+            PixelBuffer = null;
             FrameNum = 0;
         }
     }
--- OpenMB/Video/VideoTextureManager.cs
+++ OpenMB/Video/VideoTextureManager.cs
@@ -28,10 +28,11 @@ namespace OpenMB.Video
         }
         public void CreateVideoTexture(SceneManager scm,float width, float height,string aviFileName)
         {
-            //if (videotex == null)
-            //{
-                videotex = new VideoTexture(scm, width, height, aviFileName);
-            //}
+            if (videotex != null)
+            {
+                videotex.Dispose();
+            }
+            videotex = new VideoTexture(scm, width, height, aviFileName);
         }
 
         public void DestroyVideoTexture()
@@ -42,35 +43,36 @@ namespace OpenMB.Video
 
         public void Update(float timeSinceLastFrame)
         {
-            if (videotex != null)
+            if (videotex == null)
             {
-                if (videotex.FrameNum >= videotex.Stream.CountFrames)
-                {
-                    videotex.FrameNum = 0;
-                }
-                System.Drawing.Bitmap bitmap = videotex.Stream.GetBitmap(videotex.FrameNum);
-                MemoryStream ms = new MemoryStream();
-                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
-                ms.Position = 0;
-                try
-                {
-                    Image image = new Image();
-                    image.Load(Utilities.Helper.StreamToDataPtr(ms));
-                    image.FlipAroundX();
-                    videotex.PixelBuffer.BlitFromMemory(image.GetPixelBox());
-                    image.Dispose();
-                    ms.Close();
-                    ms.Dispose();
-                    videotex.FrameNum++;
-                }
-                catch (Exception ex)
-                {
-                    GameManager.Instance.log.LogMessage("[Engine Warning]: Image Data Exception. Detals:" + ex.ToString());
-                }
-                finally
-                {
-                    videotex.FrameNum++;
-                }
+                return;
+            }
+            if (videotex.FrameNum >= videotex.Stream.CountFrames)
+            {
+                videotex.FrameNum = 0;
+            }
+            System.Drawing.Bitmap bitmap = videotex.Stream.GetBitmap(videotex.FrameNum);
+            MemoryStream ms = new MemoryStream();
+            bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            ms.Position = 0;
+            try
+            {
+                Image image = new Image();
+                image.Load(Utilities.Helper.StreamToDataPtr(ms));
+                image.FlipAroundX();
+                videotex.PixelBuffer.BlitFromMemory(image.GetPixelBox());
+                image.Dispose();
+                ms.Close();
+                ms.Dispose();
+                videotex.FrameNum++;
+            }
+            catch (Exception ex)
+            {
+                GameManager.Instance.log.LogMessage("[Engine Warning]: Image Data Exception. Detals:" + ex.ToString());
+            }
+            finally
+            {
+                videotex.FrameNum++;
             }
         }
     }
