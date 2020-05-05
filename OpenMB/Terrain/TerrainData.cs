e934001d70ea9e03955df50d6abbb65bfd1d753e
Cook Green <dell20091234@gmail.com>
2020-05-05 10:16:37 +0800

    Add Terrain Generator

---
+++ OpenMB/Terrain/TerrainData.cs
@@ -0,0 +1,23 @@
+﻿using Mogre;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMB.Terrain
+{
+    public class TerrainData
+    {
+        private TerrainGroup terrainGroup;
+        private TerrainGlobalOptions terrainGlobalOptions;
+
+        public TerrainGroup TerrainGroup { get { return terrainGroup; } }
+        public TerrainGlobalOptions TerrainGlobalOptions { get { return terrainGlobalOptions; } }
+        public TerrainData(TerrainGroup terrainGroup, TerrainGlobalOptions terrainGlobalOptions)
+        {
+            this.terrainGroup = terrainGroup;
+            this.terrainGlobalOptions = terrainGlobalOptions;
+        }
+    }
+}
