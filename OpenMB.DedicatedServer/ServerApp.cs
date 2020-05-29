f6587ec080b69dd5900ffa81856bf5241ea889ff
Cook Green <dell20091234@gmail.com>
2020-05-29 14:47:19 +0800

    Add Game Sever

---
+++ OpenMB.DedicatedServer/ServerApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMB.DedicatedServer
+{
+	public class ServerApp
+	{
+	}
+}
