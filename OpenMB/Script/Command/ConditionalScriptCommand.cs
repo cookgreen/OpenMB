8f95fe93eef80d62ef0d265bae059dd22bfe3038
Cook Green <dell20091234@gmail.com>
2020-09-05 21:11:16 +0800

    Add Conditional Script Command

---
+++ OpenMB/Script/Command/ConditionalScriptCommand.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMB.Script.Command
+{
+    public class ConditionalScriptCommand : ScriptCommand
+    {
+        private string[] commandArgs; 
+        public override ScriptCommandType CommandType
+        {
+            get { return ScriptCommandType.Block; }
+        }
+
+        public override string[] CommandArgs
+        {
+            get { return commandArgs; }
+        }
+
+        public string ConditionStr { get { return CommandArgs[0]; } }
+
+        public bool CheckCondition() 
+        {
+            //TODO: Check the conditional expression
+            return true; 
+        }
+
+        public ConditionalScriptCommand()
+        {
+            commandArgs = new string[]
+            {
+                "condition statement"
+            };
+        }
+
+        public override void Execute(params object[] executeArgs)
+        {
+            foreach(var command in SubCommands)
+            {
+                command.Execute(executeArgs);
+            }
+        }
+    }
+    public class ConditionalNoStatementScriptCommand : ScriptCommand
+    {
+        private string[] commandArgs;
+        public override ScriptCommandType CommandType
+        {
+            get { return ScriptCommandType.Block; }
+        }
+
+        public override string[] CommandArgs
+        {
+            get { return commandArgs; }
+        }
+
+        public ConditionalNoStatementScriptCommand()
+        {
+            commandArgs = new string[] { };
+        }
+
+        public override void Execute(params object[] executeArgs)
+        {
+            foreach (var command in SubCommands)
+            {
+                command.Execute(executeArgs);
+            }
+        }
+    }
+}
---
+++ OpenMB/Script/Command/IfScriptCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenMB.Script.Command
+{
+    public class IFScriptCommand : ConditionalScriptCommand
+    {
+        public override string CommandName { get { return "if"; } }
+
+        public override void Execute(params object[] executeArgs)
+        {
+            //Check the condition, if success, the execute `if` block
+            //If failed, then search the SubCommands, find the `elseif` block and check one by one
+            //If no matched the `elseif` block, the find the `else` block and execute
+            if (CheckCondition())
+            {
+                var commands = SubCommands.Where(o => !(o is ElseIfScriptCommand) && !(o is ElseScriptCommand));
+                foreach (var command in commands)
+                {
+                    command.Execute(executeArgs);
+                }
+            }
+            else
+            {
+                var elseIfCommands = SubCommands.Where(o => o is ElseIfScriptCommand);
+                foreach (var command in elseIfCommands)
+                {
+                    if ((command as ConditionalScriptCommand).CheckCondition())
+                    {
+                        command.Execute(executeArgs);
+                        return;
+                    }
+                }
+
+                var elseCommand = SubCommands.Where(o => o is ElseScriptCommand).FirstOrDefault();
+                if (elseCommand != null)
+                {
+                    elseCommand.Execute(executeArgs);
+                }
+            }
+        }
+    }
+    public class ElseIfScriptCommand : ConditionalScriptCommand
+    {
+        public override string CommandName { get { return "elseif"; } }
+    }
+    public class ElseScriptCommand : ConditionalNoStatementScriptCommand
+    {
+        public override string CommandName { get { return "else"; } }
+    }
+}
