2dcc2b011d093c2ef7e073b0e260aa68dc3b93e6
Cook Green <dell20091234@gmail.com>
2021-10-06 12:10:28 +0800

    Motion Check

---
+++ OpenMB/Game/Motion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMB.Game
+{
+    public class Motion
+    {
+        private Character character;
+
+        public Motion(Character character)
+        {
+            this.character = character;
+        }
+
+        public string GetSkeletonAnimationName()
+        {
+            //Return current character skeleton animation
+            return string.Empty;
+        }
+    }
+}
---
+++ OpenMB/Game/MotionManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMB.Game
+{
+    public enum MotionState
+    {
+        NOTHING,
+        BLOCK_HAPPENED
+    }
+
+    public class MotionCheckResult
+    {
+        public MotionState ms { get; set; }
+        public Motion MotionHappened { get; set; }
+
+        public MotionCheckResult()
+        {
+            ms = MotionState.NOTHING;
+            MotionHappened = null;
+        }
+    }
+
+    public class MotionManager
+    {
+        private static MotionManager instance;
+        public static MotionManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new MotionManager();
+                }
+                return instance;
+            }
+        }
+
+        public MotionCheckResult CheckMotionColide(Motion motion1, Motion motion2)
+        {
+            MotionCheckResult motionCheckResult = new MotionCheckResult();
+
+            string motion1Name = GetMotionNameBySkeletonAnimation(motion1.GetSkeletonAnimationName());
+            string motion2Name = GetMotionNameBySkeletonAnimation(motion2.GetSkeletonAnimationName());
+
+            if (motion1Name == "AttackUpper" && motion2Name == "BlockUpper")
+            {
+                motionCheckResult.ms = MotionState.BLOCK_HAPPENED;
+                motionCheckResult.MotionHappened = motion2;
+            }
+            else if(motion1Name == "AttackRight" && motion2Name == "BlockRight")
+            {
+                motionCheckResult.ms = MotionState.BLOCK_HAPPENED;
+                motionCheckResult.MotionHappened = motion2;
+            }
+            else if(motion1Name == "AttackLeft" && motion2Name == "BlockLeft")
+            {
+                motionCheckResult.ms = MotionState.BLOCK_HAPPENED;
+                motionCheckResult.MotionHappened = motion2;
+            }
+            else if(motion1Name == "AttackCentral" && motion2Name == "BlockCentral")
+            {
+                motionCheckResult.ms = MotionState.BLOCK_HAPPENED;
+                motionCheckResult.MotionHappened = motion2;
+            }
+
+            else if (motion2Name == "AttackUpper" && motion1Name == "BlockUpper")
+            {
+                motionCheckResult.ms = MotionState.BLOCK_HAPPENED;
+                motionCheckResult.MotionHappened = motion2;
+            }
+            else if (motion2Name == "AttackRight" && motion1Name == "BlockRight")
+            {
+                motionCheckResult.ms = MotionState.BLOCK_HAPPENED;
+                motionCheckResult.MotionHappened = motion1;
+            }
+            else if (motion2Name == "AttackLeft" && motion1Name == "BlockLeft")
+            {
+                motionCheckResult.ms = MotionState.BLOCK_HAPPENED;
+                motionCheckResult.MotionHappened = motion1;
+            }
+            else if (motion2Name == "AttackCentral" && motion1Name == "BlockCentral")
+            {
+                motionCheckResult.ms = MotionState.BLOCK_HAPPENED;
+                motionCheckResult.MotionHappened = motion1;
+            }
+
+            return motionCheckResult;
+        }
+
+        private string GetMotionNameBySkeletonAnimation(string skeletonAnimName)
+        {
+            return skeletonAnimName;
+        }
+    }
+}
