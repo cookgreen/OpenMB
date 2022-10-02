788c941a54308ad333c70af7cd14704050ffb18c
DoDoCat <dell20091234@gmail.com>
2022-10-02 13:58:10 +0800

    translate.google.cn is not available

--- ThirdParty/GoogleTranslateAPI/GoogleTranslateAPIRequest.cs
+++ ThirdParty/GoogleTranslateAPI/GoogleTranslateAPIRequest.cs
@@ -13,8 +13,8 @@ namespace com.google.translate.api
     public class GoogleTranslateAPIRequest
     {
         private bool isAuto = false;
-        private string baseUrl = "http://translate.google.cn/translate_a/single?client=gtx&dt=t&dj=1&ie=UTF-8&sl={0}&tl={1}&q={2}";
-        private string baseUrlAuto = "http://translate.google.cn/translate_a/single?client=gtx&dt=t&dj=1&ie=UTF-8&sl=auto&tl={0}&q={1}";
+        private string baseUrl = "https://translate.google.com/translate_a/single?client=gtx&dt=t&dj=1&ie=UTF-8&sl={0}&tl={1}&q={2}";
+        private string baseUrlAuto = "https://translate.google.com/translate_a/single?client=gtx&dt=t&dj=1&ie=UTF-8&sl=auto&tl={0}&q={1}";
         private string srcLangID;
         private string destLangID;
 
